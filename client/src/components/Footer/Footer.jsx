c8aa58bb864cde13948c5cae769260f9d21682b3
leehaim8 <leehaim811@gmail.com>
2025-04-08 11:38:34 +0300

    Adding a footer to the login and registration page

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+// Footer.jsx
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    const links = [
+        "FAQ",
+        "Help Center",
+        "Netflix Shop",
+        "Terms of Use",
+        "Privacy",
+        "Cookie Preferences",
+        "Corporate Information",
+        "Do Not Sell or Share My Personal Information",
+        "Ad Choices",
+    ];
+
+    return (
+        <footer >
+            <div className="footer-top">
+                <p>Questions? Call <span className="phone-number">1-844-505-2993</span></p>
+            </div>
+
+            <div className="footer-links">
+                {links.map((text, index) => (
+                    <span key={index} className="footer-link">{text}</span>
+                ))}
+            </div>
+
+            <div className="footer-bottom">
+                <select className="language-select">
+                    <option>English</option>
+                </select>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
