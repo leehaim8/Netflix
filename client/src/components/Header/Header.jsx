283c36f76e221329e2630eac3b4578703ed1b8a5
leehaim8 <leehaim811@gmail.com>
2025-04-13 19:06:45 +0300

    Fixing the menu in the header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,14 +3,26 @@ import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function Header(props) {
+    const location = useLocation();
+    const pathSegments = location.pathname.split('/');
+    const profileId = pathSegments[pathSegments.length - 1];
     const [showDropdown, setShowDropdown] = useState(false);
     const navigate = useNavigate();
 
+    const navLinks = [
+        { label: 'Home', path: `/homepage/${profileId}` },
+        { label: 'TV Shows', path: `/tvShows/${profileId}` },
+        { label: 'Movies', path: `/movies/${profileId}` },
+        { label: 'New & Popular', path: '/new-popular' },
+        { label: 'My List', path: '/my-list' },
+        { label: 'Browse' },
+    ];
+
     const handleProfileClick = () => {
-        setShowDropdown((prev) => !prev);
+        setShowDropdown(prev => !prev);
     };
 
     const handleSignOut = () => {
@@ -23,27 +35,32 @@ function Header(props) {
     return (
         <nav>
             <div className="header-navbar-left">
-                <div className="header-logo"></div>
+                <div className="header-logo" onClick={() => navigate('/home')}></div>
                 <ul className="header-nav-links">
-                    <li className="header-active">Home</li>
-                    <li>TV Shows</li>
-                    <li>Movies</li>
-                    <li>New & Popular</li>
-                    <li>My List</li>
-                    <li>Browse</li>
+                    {navLinks.map((link,index) => (
+                        <li
+                            key={index}
+                            className={location.pathname === link.path ? 'header-active' : ''}
+                            onClick={() => navigate(link.path)}
+                        >
+                            {link.label}
+                        </li>
+                    ))}
                 </ul>
             </div>
             <div className="header-navbar-right">
                 <SearchIcon className="header-icon" />
                 <NotificationsNoneIcon className="header-icon" />
                 <div className="header-profile" onClick={handleProfileClick}>
-                    <img src={`http://localhost:8080/Public/${props.image}`} alt="Profile" className="header-avatar-profile" />
+                    <img
+                        src={`http://localhost:8080/Public/${props.image}`}
+                        alt="Profile"
+                        className="header-avatar-profile"
+                    />
                     <ArrowDropDownIcon className="header-dropdown-icon" />
                     {showDropdown && (
                         <div className="header-dropdown-menu">
-                            <p>Manage Profiles</p>
-                            <p>Account</p>
-                            <p>Help Center</p>
+                            <p onClick={() => navigate(`/userprofile/${profileId}`)}>Manage Profiles</p>
                             <p onClick={handleSignOut}>Sign out</p>
                         </div>
                     )}
