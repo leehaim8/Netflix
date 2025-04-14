ec05d00e08ae7df43f49b619bf7c5c6915431932
leehaim8 <leehaim811@gmail.com>
2025-04-14 17:15:36 +0300

    Update the routes in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './SingIn/SingIn';
 import Register from './SingUp/SingUp';
+import UserProfile from './UserProfile/UserProfile';
+import ProtectedRoute from './ProtectedRoute';
+import HomePage from './HomePage/HomePage';
+import TvShows from './TvShows';
+import Movies from './Movies';
+import NewAndPopular from './NewAndPopular/NewAndPopular';
 
 function App() {
   return (
@@ -9,6 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Register />} />
+        <Route path="/userprofile/:userId" element={<ProtectedRoute> <UserProfile /> </ProtectedRoute>} />
+        <Route path="/homepage/:id" element={<ProtectedRoute> <HomePage /> </ProtectedRoute>} />
+        <Route path="/tvShows/:id" element={<ProtectedRoute> <TvShows /> </ProtectedRoute>} />
+        <Route path="/movies/:id" element={<ProtectedRoute> <Movies /> </ProtectedRoute>} />
+        <Route path="/newAndPopular/:id" element={<ProtectedRoute> <NewAndPopular /> </ProtectedRoute>} />
       </Routes>
     </Router>
   );
